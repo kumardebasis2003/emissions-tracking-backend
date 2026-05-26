7aff8fa134d59a32e415604bc05f4671483cad02
kumardebasis2003 <debasiskumarghadai2003@gmail.com>
2026-05-26 23:31:13 +0530

    hello

--- build.sh
+++ build.sh
@@ -2,6 +2,27 @@
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-
 python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth.models import User
+
+username = "admin"
+
+password = "admin123"
+
+email = "admin@gmail.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+
+    print("Superuser created")
+
+else:
+    print("Superuser already exists")
+
+END
